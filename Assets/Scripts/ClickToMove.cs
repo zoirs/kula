65ae1ca240172555bc136a53d76ccbd208e1f58f
zoirs <zoirs@ya.ru>
2018-04-07 17:56:28 +0300

    delete unused files

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -15,28 +15,11 @@ public class ClickToMove : MonoBehaviour
 
     void Update()
     {
-
-        if (Input.GetKeyDown ("space")) {
-        
-            Debug.Log("1111111");
-        }
-
-        {
-        }
-
         if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
             {
-//                Vector3 position = m_Agent.gameObject.transform.position;
-//                Vector3 destination = new Vector3(position.x + 5, position.y, position.z);
-//                
-//                if (!m_Agent.SetDestination(destination))
-//                {
-//                    destination = new Vector3(position.x + 5, position.y - 5, position.z);
-//                    m_Agent.SetDestination(destination);
-//                }
                 m_Agent.destination = m_HitInfo.point;
             }
         }
