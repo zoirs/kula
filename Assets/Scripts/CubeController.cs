922fc697bf4cf18e54aaeecfe5b4c7df49be9d97
zoirs <zoirs@ya.ru>
2018-06-10 11:56:09 +0300

    удалил кубы

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -1,32 +1,9 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.AI;
+﻿using UnityEngine;
 
-[Obsolete("Похоже пока нет нужды")]
 public class CubeController : MonoBehaviour {
-	private NavMeshLink[] _angelLinks;
-	private NavMeshLink[] _surfaceLinks;
 	public Vector3 Position { get; private set; }
 
-	// Use this for initialization
 	void Start () {
 		Position = gameObject.transform.position;
-		
-		GameObject angelLinks = GameObject.Find("Angel");
-		_angelLinks = angelLinks.GetComponentsInChildren<NavMeshLink>();
-		
-		GameObject surfaceLinks = GameObject.Find("Angel");
-		_surfaceLinks = surfaceLinks.GetComponentsInChildren<NavMeshLink>();
-	}
-
-	public void DisableLinks() {
-		foreach (NavMeshLink link in _angelLinks) {
-			link.enabled = false;
-		}
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
